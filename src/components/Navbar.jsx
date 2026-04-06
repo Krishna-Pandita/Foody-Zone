04fb156f9c70706dfba52b051b79e1310373bc06
Krishna Pandita <kpandita68@gmail.com>
2026-04-06 14:07:59 +0530

    updated search box

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   return(
-    <div className="w-full h-[100px] bg-red-300 flex justify-between items-center px-9">
+    <div className="w-full h-[100px] flex justify-between items-center px-9">
 
 <div className="h-[60px] w-[60px] bg-white flex justify-center items-center rounded-3xl shadow-xl">
-  <MdFastfood className="w-[30px] h-[30px] text-green-500 "/>
+  <MdFastfood className="w-[30px] h-[30px] text-green-600 "/>
 </div>
 
 <div>
-  <form>
-    <FaSearch />
-    <input placeholder="Search Your Food"></input>
+  <form className="w-[700px] h-[60px] bg-white flex items-center px-7 gap-3 rounded-lg shadow-lg ">
+    <FaSearch className="h-[18px] w-[18px] text-green-600" />
+    <input placeholder="Search Your Food" className="outline-none text-[15px] text-[17px]"></input>
   </form>
    </div>
 
 
 <div className="h-[60px] w-[60px] bg-white flex justify-center items-center rounded-3xl shadow-xl">
-<FaShoppingCart className="w-[30px] h-[30px] text-green-500" />
+<FaShoppingCart className="w-[30px] h-[30px] text-green-600" />
 </div>
 
 
