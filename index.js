eedfbbe10a35a93618b2fe1602a2ff1074a2f216
Krishna Pandita <kpandita68@gmail.com>
2026-04-09 15:12:29 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import "./index.css";  
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./src/pages/Home";   // ✅ IMPORT HOME
+
+import Home from "./src/pages/Home";   // ✅ CORRECT
+import UserContextProvider from "./src/contextApi/UserContext"; // ✅ CORRECT
 
 const App = () => {
-  return <Home />;   // ✅ RENDER HOME
+  return <Home />;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-
+root.render(
+  <UserContextProvider>
+    <App />
+  </UserContextProvider>
+);
